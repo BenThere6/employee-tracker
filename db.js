242a96b875d62e59488032ad48aa1238d30be7b7
BenThere <benjaminbirdsall@icloud.com>
2023-09-19 16:42:19 -0600

    rename connection to pool

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const pool = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 })
 
-module.exports = db;
+module.exports = pool;
