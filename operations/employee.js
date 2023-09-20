020d487668e1741f865b4915d5705d1597f35f6e
BenThere <benjaminbirdsall@icloud.com>
2023-09-19 18:28:01 -0600

    view employees also shows their dept and salary

--- operations/employee.js
+++ operations/employee.js
@@ -8,12 +8,13 @@ function getAllEmployees() {
       }
       
       const query = `
-        SELECT e.id, e.first_name, e.last_name, r.title AS role, 
-               CONCAT(m.first_name, ' ', m.last_name) AS manager
+        SELECT e.id, e.first_name, e.last_name, r.title AS role, r.salary, 
+                d.name AS department, CONCAT(m.first_name, ' ', m.last_name) AS manager
         FROM employee e
         LEFT JOIN role r ON e.role_id = r.id
+        LEFT JOIN department d ON r.department_id = d.id
         LEFT JOIN employee m ON e.manager_id = m.id
-      `;
+        `;
 
       connection.query(query, (queryError, results) => {
         connection.release();
